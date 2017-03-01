9e7382bf69136732d20569dc863b58ad36953877
Ekaterina7 <mmmkatia@bk.ru>
2017-03-01 10:51:48 +0500

    add

--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -10,6 +10,46 @@ namespace palindrome
     {
         static void Main(string[] args)
         {
+
+
+
+        }
+        static int Counter(int chislo_tmp)
+        {
+            int s = 0;
+            while (chislo_tmp != 0)
+            {
+
+                int c = chislo_tmp % 10;
+
+                s++;
+                chislo_tmp = chislo_tmp / 10;
+            }
+            return s;
+        }
+
+        static int Power10(int s)
+        {
+            int pow = (s / 2);
+            return pow;
+
+        }
+
+        static int FirstHalfChisla(int chislo, int pow)
+        {
+
+            int half1 = (int)(chislo / Math.Pow(10, pow));
+            return half1;
+
+        }
+
+        static int SecondHalfChisla(int chislo, int pow)
+        {
+
+            int half2 = (int)(chislo % Math.Pow(10, pow));
+            return half2;
+
         }
     }
+
 }
