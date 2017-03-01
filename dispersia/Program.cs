1153d4fbdbf44c376d8ea1474b35286004c201c9
Ekaterina7 <mmmkatia@bk.ru>
2017-03-01 10:20:27 +0500

    write kod

--- dispersia/Program.cs
+++ dispersia/Program.cs
@@ -12,19 +12,51 @@ namespace dispersia
         {
             int[] mas;
             mas = new int[7];
+         int mid =    MiddlZnachenie(mas);
+         Console.WriteLine(mid);
+         Dispersia(mas);
+            
 
         }
 
+        static int[] ZapolnenieMass(int[] mas, int start, int finish)
+        {
+            Random gen = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = gen.Next(start, finish);
+            }
+            return mas;
+
+        }
+
+
         static int MiddlZnachenie(int[] mas)
         {
-
-            int summ;
+            mas = ZapolnenieMass(mas, 2, 9);
+            int summ=0;
             for (int i = 0; i < mas.Length; i++)
             {
                 summ = summ + mas[i];
             }
+            return summ / mas.Length;
 
         }
+
+        static void Dispersia(int[] mas)
+        {
+            int mid = MiddlZnachenie(mas);
+            int dis;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                dis = Math.Abs(mas[i] - mid);
+                Console.WriteLine("дисперсия {0} = {1} ", i, dis);
+            }
+
+
+
+        }
+
 
 
     }
